a787e836039be99ea0a9a3d1baf1cbc52c017e3c
Dorian Scholz <DorianScholz@gmx.de>
2012-08-09 19:06:18 +0200

    added qt_gui_py_common package for common python code not specific to ROS

---
+++ qt_gui_py_common/src/qt_gui_py_common/worker_thread.py
@@ -0,0 +1,57 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, Dorian Scholz
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import qt_gui.qt_binding_helper
+from QtCore import QThread
+
+class WorkerThread(QThread):
+    
+    """Convenience wrapper around QThread allowing to easily run code in a separate thread, 
+    get notified upon completion and kill the thread synchronously."""
+
+    def __init__(self, run_callback, finished_callback=None):
+        super(WorkerThread, self).__init__()
+        self._run_callback = run_callback
+        self._finished_callback = finished_callback
+
+    def start(self):
+        self.run = self._run_callback
+        if self._finished_callback is not None:
+            self.finished.connect(self._finished_callback)
+        super(WorkerThread, self).start()
+
+    def kill(self):
+        if self.isRunning():
+            if self._finished_callback is not None:
+                self.finished.disconnect(self._finished_callback)
+            self.terminate()
+            self.wait()
