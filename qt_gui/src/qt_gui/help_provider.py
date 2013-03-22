c686e6a2c80bb37c03843cc8dbdf1fc9595e9ac9
Dorian Scholz\n <DorianScholz@gmx.de>
2013-03-22 17:42:35 +0100

    changed help_provider to use rospkg if available

--- qt_gui/src/qt_gui/help_provider.py
+++ qt_gui/src/qt_gui/help_provider.py
@@ -31,8 +31,11 @@
 import webbrowser
 
 from python_qt_binding.QtCore import QObject, Slot
-from roslib.manifest import load_manifest
-
+try:
+    import rospkg
+    load_manifest = rospkg.RosPack().get_manifest
+except ImportError:
+    from roslib.manifest import load_manifest
 
 class HelpProvider(QObject):
 
