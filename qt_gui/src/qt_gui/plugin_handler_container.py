4db8098b320f1e18d50f7682020e8604e908f92e
Samuel Bachmann <samuel.bachmann@gmail.com>
2015-10-27 06:34:49 +0100

    minor update

--- qt_gui/src/qt_gui/plugin_handler_container.py
+++ qt_gui/src/qt_gui/plugin_handler_container.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011, Dirk Thomas, Dorian Scholz, TU Darmstadt, Samuel Bachmann
+# Copyright (c) 2011, Dirk Thomas, Dorian Scholz, TU Darmstadt
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
