421a793723275468ee231f36be7acb5cd98b8f34
Dirk Thomas <dthomas@osrfoundation.org>
2016-04-20 12:13:40 -0700

    fix another import

--- qt_gui_py_common/src/qt_gui_py_common/console_text_edit.py
+++ qt_gui_py_common/src/qt_gui_py_common/console_text_edit.py
@@ -33,7 +33,8 @@
 import sys
 
 from python_qt_binding.QtCore import Qt
-from python_qt_binding.QtGui import QFont, QTextEdit
+from python_qt_binding.QtGui import QFont
+from python_qt_binding.QtWidgets import QTextEdit
 
 
 class ConsoleTextEdit(QTextEdit):
