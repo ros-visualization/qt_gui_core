782c50c26c78d25689bd8d7160ccb91eb9c84d6a
Stephen <brawner@gmail.com>
2018-10-24 12:37:56 -0700

    Malformed setup.py

--- qt_gui/setup.py
+++ qt_gui/setup.py
@@ -2,9 +2,11 @@
 
 from setuptools import setup
 
+package_name = 'qt_gui'
+
 d = dict(
     packages=['qt_gui'],
-    package_dir={'': 'qt_gui'}
+    package_dir={'': 'qt_gui'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
--- qt_gui_py_common/setup.py
+++ qt_gui_py_common/setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-
+package_name = 'qt_gui_py'
 d = dict(
     packages=['qt_gui_py_common'],
-    package_dir={'': 'python_qt_binding'}
+    package_dir={'': 'python_qt_binding'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
