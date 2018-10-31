85570c058ffb5ba6fea23dbf0b248db185a6f6d1
Stephen <brawner@gmail.com>
2018-10-31 12:05:37 -0700

    Fixing PR issues

--- qt_dotgraph/setup.py
+++ qt_dotgraph/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['qt_dotgraph'],
-    package_dir={'': 'qt_dotgraph'}
+    package_dir={'': 'src'}
 )
 
 setup(**d)
