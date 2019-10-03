3862b61b082c303cf87c236cdb3c34f49edf5317
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-10-03 13:56:14 -0700

    fix compile regression on armhf

--- qt_gui_cpp/include/qt_gui_cpp/ros_pluginlib_plugin_provider_for_plugin_providers.h
+++ qt_gui_cpp/include/qt_gui_cpp/ros_pluginlib_plugin_provider_for_plugin_providers.h
@@ -35,9 +35,6 @@
 
 #include "plugin_provider.h"
 #include "ros_pluginlib_plugin_provider.h"
-#include "exports.h"
-
-template class QT_GUI_CPP_DECL qt_gui_cpp::RosPluginlibPluginProvider<qt_gui_cpp::PluginProvider>;
 
 namespace qt_gui_cpp
 {
