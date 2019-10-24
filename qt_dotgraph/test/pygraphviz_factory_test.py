b66915ba505bf040b3d4384a8e98c4551d953614
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-10-24 12:54:25 -0700

    make another test more flexible in terms of whitespaces (#193)
    * make another test more flexible in terms of whitespaces
    
    * and more

--- qt_dotgraph/test/pygraphviz_factory_test.py
+++ qt_dotgraph/test/pygraphviz_factory_test.py
@@ -31,6 +31,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import re
 import unittest
 from qt_dotgraph.pygraphvizfactory import PygraphvizFactory
 
@@ -93,12 +94,15 @@ class PygraphvizFactoryTest(unittest.TestCase):
         fac.add_node_to_graph(g, 'edge')
         fac.add_edge_to_graph(g, 'foo', 'edge')
         fac.add_subgraph_to_graph(g, 'graph')
-        snippets = ['strict digraph {\n\tgraph',
+        snippets = ['strict digraph { graph',
                     'foo',
                     'label=foo',
                     '"edge"',
                     'label="edge"',
                     'foo -> "edge"']
         result = fac.create_dot(g)
+        # get rid of version specific quotes / whitespaces
+        result = re.sub('""', ' ', result)
+        result = re.sub('[\n\t ]+', ' ', result)
         for sn in snippets:
             self.assertTrue(sn in result, '%s \nmissing in\n %s' % (sn, result))
