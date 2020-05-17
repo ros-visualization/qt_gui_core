ros-visualization/qt_gui_core

commit a023298f4cb09c4a41636384997fd858a1ac8374

Changes for flake8 and ROS2 compatible style changes

qt_gui_py_common/src/qt_gui_py_common/exclusive_options_group.py
@@ -30,15 +30,16 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from python_qt_binding.QtWidgets import QButtonGroup, QGroupBox, QLabel, QRadioButton, QVBoxLayout, QWidget
+from python_qt_binding.QtWidgets import \
+    QButtonGroup, QGroupBox, QLabel, QRadioButton, QVBoxLayout
 
 
 class ExclusiveOptionGroup(QGroupBox):
-
     """
     Creates a button group of exclusive radio options.
 
-    Options must be a dict with following keys: 'enabled','selected','title','description','tooltip'
+    Options must be a dict with following keys:
+        'enabled', 'selected', 'title', 'description', 'tooltip'
     """
 
     def __init__(self, options, title='Exclusive Options', selected_index=None, parent=None):
@@ -48,7 +49,7 @@ class ExclusiveOptionGroup(QGroupBox):
         self._button_group = QButtonGroup()
         self._button_group.setExclusive(True)
         self._options = options
-        if parent == None:
+        if parent is None:
             parent = self
 
         for (button_id, option) in enumerate(self._options):
@@ -64,8 +65,12 @@ class ExclusiveOptionGroup(QGroupBox):
                 parent.layout().addWidget(QLabel(option['description']))
 
     def get_settings(self):
-        """Returns dictionary with selected_index (int) and selected_option (dict) keys."""
+        """Return dictionary with selected_index (int) and selected_option (dict) keys."""
         selected_index = self._button_group.checkedId()
         if selected_index >= 0:
-            return {'selected_index': selected_index, 'selected_option': self._options[selected_index]}
+            return
+            {
+                'selected_index': selected_index,
+                'selected_option': self._options[selected_index]
+            }
         return {'selected_index': None, 'selected_option': None}

qt_dotgraph/setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(

qt_dotgraph/src/qt_dotgraph/dot_to_qt.py
@@ -30,12 +30,9 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# work around for https://bugs.launchpad.net/ubuntu/+source/pydot/+bug/1321135
-import pyparsing
-pyparsing._noncomma = "".join([c for c in pyparsing.printables if c != ","])
-import pydot
-
 import codecs
+
+import pydot
 
 from python_qt_binding.QtCore import QPointF, QRectF
 from python_qt_binding.QtGui import QColor
@@ -74,7 +71,7 @@ class DotToQtGenerator():
         for name in subgraph.get_attributes().keys():
             value = get_unquoted(subgraph, name)
             attr[name] = value
-        obj_dic = subgraph.__getattribute__("obj_dict")
+        obj_dic = subgraph.__getattribute__('obj_dict')
         for name in obj_dic:
             if name not in ['nodes', 'attributes', 'parent_graph'] and obj_dic[name] is not None:
                 attr[name] = get_unquoted(obj_dic, name)
@@ -121,15 +118,13 @@ class DotToQtGenerator():
         return subgraph_nodeitem
 
     def getNodeItemForNode(self, node, highlight_level, scene=None):
-        """
-        returns a pyqt NodeItem object, or None in case of error or invisible style
-        """
+        """Return a pyqt NodeItem object, or None in case of error or invisible style."""
         # let pydot imitate pygraphviz api
         attr = {}
         for name in node.get_attributes().keys():
             value = get_unquoted(node, name)
             attr[name] = value
-        obj_dic = node.__getattribute__("obj_dict")
+        obj_dic = node.__getattribute__('obj_dict')
         for name in obj_dic:
             if name not in ['attributes', 'parent_graph'] and obj_dic[name] is not None:
                 attr[name] = get_unquoted(obj_dic, name)
@@ -142,7 +137,7 @@ class DotToQtGenerator():
 
         name = node.attr.get('label', node.attr.get('name'))
         if name is None:
-            print("Error, no label defined for node with attr: %s" % node.attr)
+            print('Error, no label defined for node with attr: %s' % node.attr)
             return None
 
         name = codecs.escape_decode(name)[0].decode('utf-8')
@@ -171,7 +166,8 @@ class DotToQtGenerator():
     def addEdgeItem(
             self, edge, nodes, edges, highlight_level, same_label_siblings=False, scene=None):
         """
-        adds EdgeItem by data in edge to edges
+        Add EdgeItem by data in edge to edges.
+
         :param same_label_siblings:
             if true, edges with same label will be considered siblings (collective highlighting)
         """
@@ -227,7 +223,7 @@ class DotToQtGenerator():
         if same_label_siblings:
             if label is None:
                 # for sibling detection
-                label = "%s_%s" % (source_node, destination_node)
+                label = '%s_%s' % (source_node, destination_node)
             # symmetrically add all sibling edges with same label
             if label in edges:
                 for sibling in edges[label]:
@@ -246,8 +242,9 @@ class DotToQtGenerator():
 
     def dotcode_to_qt_items(self, dotcode, highlight_level, same_label_siblings=False, scene=None):
         """
-        takes dotcode, runs layout, and creates qt items based on the dot layout.
-        returns two dicts, one mapping node names to Node_Item, one mapping edge names to lists of
+        Take dotcode, run layout, and creates qt items based on the dot layout.
+
+        Returns two dicts, one mapping node names to Node_Item, one mapping edge names to lists of
             Edge_Item.
         :param same_label_siblings:
             if true, edges with same label will be considered siblings (collective highlighting)
@@ -267,7 +264,7 @@ class DotToQtGenerator():
         return nodes, edges
 
     def parse_nodes(self, graph, highlight_level, scene=None):
-        """Recursively searches all nodes inside the graph and all subgraphs."""
+        """Recursively search all nodes inside the graph and all subgraphs."""
         # let pydot imitate pygraphviz api
         graph.nodes_iter = graph.get_node_list
         graph.subgraphs_iter = graph.get_subgraph_list
@@ -286,7 +283,8 @@ class DotToQtGenerator():
                 # hack required by pydot
                 if node.get_name() in ('graph', 'node', 'empty'):
                     continue
-                nodes[node.get_name()] = self.getNodeItemForNode(node, highlight_level, scene=scene)
+                nodes[node.get_name()] = \
+                    self.getNodeItemForNode(node, highlight_level, scene=scene)
         for node in graph.nodes_iter():
             # hack required by pydot
             if node.get_name() in ('graph', 'node', 'empty'):
@@ -295,7 +293,7 @@ class DotToQtGenerator():
         return nodes
 
     def parse_edges(self, graph, nodes, highlight_level, same_label_siblings, scene=None):
-        """Recursively searches all edges inside the graph and all subgraphs."""
+        """Recursively search all edges inside the graph and all subgraphs."""
         # let pydot imitate pygraphviz api
         graph.subgraphs_iter = graph.get_subgraph_list
         graph.edges_iter = graph.get_edge_list

qt_dotgraph/src/qt_dotgraph/edge_item.py
@@ -35,7 +35,7 @@ from python_qt_binding.QtWidgets import (QGraphicsPathItem,
                                          QGraphicsSimpleTextItem)
 
 
-from .graph_item import GraphItem
+from qt_dotgraph.graph_item import GraphItem
 
 
 class EdgeItem(GraphItem):
@@ -87,7 +87,8 @@ class EdgeItem(GraphItem):
             self._label.hoverLeaveEvent = self._handle_hoverLeaveEvent
             self._label.setAcceptHoverEvents(True)
 
-        # spline specification according to http://www.graphviz.org/doc/info/attrs.html#k:splineType
+        # spline specification according to
+        # http://www.graphviz.org/doc/info/attrs.html#k:splineType
         coordinates = spline.split(' ')
         # extract optional end_point
         end_point = None

qt_dotgraph/src/qt_dotgraph/node_item.py
@@ -38,8 +38,8 @@ from python_qt_binding.QtWidgets import (QGraphicsEllipseItem,
                                          QGraphicsRectItem,
                                          QGraphicsSimpleTextItem)
 
-from .dot_shapes import QGraphicsBox3dItem
-from .graph_item import GraphItem
+from qt_dotgraph.dot_shapes import QGraphicsBox3dItem
+from qt_dotgraph.graph_item import GraphItem
 
 
 class NodeItem(GraphItem):

qt_dotgraph/src/qt_dotgraph/pydotfactory.py
@@ -36,9 +36,6 @@ try:
 except ImportError:
     from urllib import quote
 
-# work around for https://bugs.launchpad.net/ubuntu/+source/pydot/+bug/1321135
-import pyparsing
-pyparsing._noncomma = "".join([c for c in pyparsing.printables if c != ","])
 import pydot
 
 
@@ -50,7 +47,7 @@ class PydotFactory():
 
     def escape_label(self, name):
         if name in ['graph', 'subgraph', 'node', 'edge']:
-            ret = "%s_" % name
+            ret = '%s_' % name
         else:
             ret = name
         return ret
@@ -92,7 +89,8 @@ class PydotFactory():
                           url=None,
                           tooltip=None):
         """
-        creates a node item for this factory, adds it to the graph.
+        Create a node item for this factory, adds it to the graph.
+
         Node name can vary from label but must always be same for the same node label
         """
         if nodename is None or nodename == '':
@@ -125,7 +123,8 @@ class PydotFactory():
                               style='bold',
                               subgraphlabel=None):
         """
-        creates a cluster subgraph  item for this factory, adds it to the graph.
+        Create a cluster subgraph item for this factory, adds it to the graph.
+
         cluster name can vary from label but must always be same for the same node label.
         Most layouters require cluster names to start with cluster.
         """
@@ -175,4 +174,4 @@ class PydotFactory():
         if type(dot) != str:
             dot = dot.decode()
         # sadly pydot generates line wraps cutting between numbers
-        return dot.replace("\\\n", "")
+        return dot.replace('\\\n', '')

qt_dotgraph/src/qt_dotgraph/pygraphvizfactory.py
@@ -56,7 +56,8 @@ class PygraphvizFactory():
                           url=None,
                           tooltip=None):
         """
-        creates a node item for this factory, adds it to the graph.
+        Create a node item for this factory, adds it to the graph.
+
         Node name can vary from label but must always be same for the same node label
         """
         if nodename is None or nodename == '':
@@ -84,15 +85,16 @@ class PygraphvizFactory():
                               shape='box',
                               style='bold'):
         """
-        creates a cluster subgraph  item for this factory, adds it to the graph.
-        cluster name can vary from label but must always be same for the same node label.
+        Create a cluster subgraph  item for this factory, adds it to the graph.
+
+        Cluster name can vary from label but must always be same for the same node label.
         Most layouters require cluster names to start with cluster.
         """
         if subgraphlabel is None or subgraphlabel == '':
             raise ValueError('Empty subgraph label')
 
         sg = graph.add_subgraph(
-            name="cluster_%s" % subgraphlabel, ranksep=ranksep, rankdir=rankdir,
+            name='cluster_%s' % subgraphlabel, ranksep=ranksep, rankdir=rankdir,
             rank=rank, compound=compound, label=str(subgraphlabel), style=style, color=color)
 
         return sg
@@ -109,4 +111,4 @@ class PygraphvizFactory():
     def create_dot(self, graph):
         graph.layout('dot')
         # sadly pygraphviz generates line wraps cutting between numbers
-        return graph.string().replace("\\\n", "")
+        return graph.string().replace('\\\n', '')

qt_gui/src/qt_gui/about_handler.py
@@ -40,10 +40,10 @@ from .ros_package_helper import get_package_path
 
 
 class AboutHandler(QObject):
-
     """
-    Handler for the about action in the menu bar showing a message box with details on the used
-    libraries and their versions.
+    Handler for the 'About action' menu bar item.
+
+    This handler shows a message box with details on the used libraries and their versions.
     """
 
     def __init__(self, qtgui_path, parent=None):

commit 61347784fbac55bb80d4f5472351ed6bea0d3098

ExclusiveOptionGroup: Fix return value of get_settings()
Currently it returns `None` if the selected index was valid, which completely breaks the functionality. Looks like the bug was introduced in a023298 while making some formatting changes.

qt_gui_py_common/src/qt_gui_py_common/exclusive_options_group.py
@@ -68,8 +68,7 @@ class ExclusiveOptionGroup(QGroupBox):
         """Return dictionary with selected_index (int) and selected_option (dict) keys."""
         selected_index = self._button_group.checkedId()
         if selected_index >= 0:
-            return
-            {
+            return {
                 'selected_index': selected_index,
                 'selected_option': self._options[selected_index]
             }