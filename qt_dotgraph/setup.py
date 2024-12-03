8cce056dce4b526b2666c54fbc472000522cf8f8
Chris Lalancette <clalancette@gmail.com>
2024-12-03 22:37:50 +0000

    Convert qt_dotgraph to a pure Python package.
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- qt_dotgraph/setup.py
+++ qt_dotgraph/setup.py
@@ -1,12 +1,32 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+package_name = 'qt_dotgraph'
 
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['qt_dotgraph'],
-    package_dir={'': 'src'}
+setup(
+    name=package_name,
+    version='2.8.3',
+    packages=[package_name],
+    package_dir={'': 'src'},
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Thibault Kruse',
+    maintainer='Chris Lalancette',
+    maintainer_email='clalancette@gmail.com',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'qt_dotgraph provides helpers to work with dot graphs.'
+    ),
+    license='BSD',
+    tests_require=['pytest'],
 )
-
-setup(**d)
