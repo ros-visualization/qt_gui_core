a4a5c6dddf83d89f9553abb16423e1b869d534f6
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-09 14:23:55 +0200

    2.9.2

--- qt_dotgraph/setup.py
+++ qt_dotgraph/setup.py
@@ -4,7 +4,7 @@ package_name = 'qt_dotgraph'
 
 setup(
     name=package_name,
-    version='2.9.1',
+    version='2.9.2',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
