561350a66d99cf59167ea19abdfe1ebbed534962
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2026-02-19 16:03:02 +0100

    fix(qt_gui): __builtin__ -> builtins

--- qt_gui/src/qt_gui/reload_importer.py
+++ qt_gui/src/qt_gui/reload_importer.py
@@ -30,7 +30,7 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import __builtin__
+import builtins
 from importlib import reload
 import os
 import sys
@@ -51,10 +51,10 @@ class ReloadImporter:
         self._import = __builtin__.__import__
 
     def enable(self):
-        __builtin__.__import__ = self._reimport
+        builtins.__import__ = self._reimport
 
     def disable(self):
-        __builtin__.__import__ = self._import
+        builtins.__import__ = self._import
 
     def add_reload_path(self, path):
         if self._reload_paths is None:
