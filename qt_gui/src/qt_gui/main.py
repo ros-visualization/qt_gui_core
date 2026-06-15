5a127fd87c3c6ca2fcb0d22eaeb026110c7ff6c9
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-15 11:32:49 +0200

    Removed Python2 references
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- qt_gui/src/qt_gui/main.py
+++ qt_gui/src/qt_gui/main.py
@@ -30,8 +30,6 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import print_function
-
 from argparse import ArgumentParser, SUPPRESS
 import os
 import signal
