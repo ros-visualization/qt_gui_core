b9da3b91578dd3888ad5d5359ea66ae73ec0a17e
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-25 21:41:15 +0200

    Use smart ptr with class loader
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- qt_gui_cpp/include/qt_gui_cpp/ros_pluginlib_plugin_provider.hpp
+++ qt_gui_cpp/include/qt_gui_cpp/ros_pluginlib_plugin_provider.hpp
@@ -75,16 +75,14 @@ public:
     , PluginProvider()
     , export_tag_(export_tag)
     , base_class_type_(base_class_type)
-    , class_loader_(0)
+    , class_loader_(nullptr)
   {
     unload_libraries_event_ = QEvent::registerEventType();
   }
 
   virtual ~RosPluginlibPluginProvider()
   {
-    if (class_loader_) {
-      delete class_loader_;
-    }
+    // class_loader_ (std::unique_ptr) is released automatically.
   }
 
   virtual QMultiMap<QString, QString> discover(QObject * discovery_data)
@@ -94,10 +92,6 @@ public:
 
   virtual QList<PluginDescriptor *> discover_descriptors(QObject * discovery_data)
   {
-    if (class_loader_) {
-      delete class_loader_;
-    }
-
     Settings discovery_settings(discovery_data);
     QString key = "qt_gui_cpp.RosPluginlibPluginProvider/" + export_tag_ + " " + base_class_type_;
     bool is_cached = discovery_settings.contains(key);
@@ -115,7 +109,7 @@ public:
           "and base class '%s'",
           export_tag_.toStdString().c_str(), base_class_type_.toStdString().c_str());
     }
-    class_loader_ = new pluginlib::ClassLoader<T>(export_tag_.toStdString(),
+    class_loader_ = std::make_unique<pluginlib::ClassLoader<T>>(export_tag_.toStdString(),
         base_class_type_.toStdString(), std::string("plugin"), plugin_xml_paths);
 
     if (!is_cached) {
@@ -405,7 +399,7 @@ private:
 
   int unload_libraries_event_;
 
-  pluginlib::ClassLoader<T> * class_loader_;
+  std::unique_ptr<pluginlib::ClassLoader<T>> class_loader_;
 
   QMap<void *, std::shared_ptr<T>> instances_;
 
